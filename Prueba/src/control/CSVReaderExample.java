0d1adc33f6b847d3f0948bceb782f5494b787698
VictorGreciano <victor.greciano@juanxxiii.net>
2023-11-07 16:44:04 +0100

    Hika

--- Prueba/src/control/CSVReaderExample.java
+++ Prueba/src/control/CSVReaderExample.java
@@ -29,7 +29,7 @@ public class CSVReaderExample {
             }
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
+        } finally { 
             try {
                 if (reader != null) {
                     reader.close();
