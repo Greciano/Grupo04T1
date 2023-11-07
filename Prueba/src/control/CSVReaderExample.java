ec1b5130840271d0c802bdfcb74508364f45b58e
VictorGreciano <victor.greciano@juanxxiii.net>
2023-11-07 16:08:21 +0100

    Victor

--- Prueba/src/control/CSVReaderExample.java
+++ Prueba/src/control/CSVReaderExample.java
@@ -3,23 +3,29 @@ package control;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class CSVReaderExample {
 
     public static void main(String[] args) {
-        String file = "C:\\Users\\byhol\\Downloads\\archive\\vgsales.csv";
+        String file = "D:\\LucaTicSteam\\Grupo04T1\\WKLucaSteamVG/vgsales.csv";
 
         BufferedReader reader = null;
         String line = "";
+        ArrayList<String[]> data = new ArrayList<>();
 
         try {
             reader = new BufferedReader(new FileReader(file));
             while ((line = reader.readLine()) != null) {
                 String[] row = line.split(",");
+                data.add(row);
+            }
+
+            for (String[] row : data) {
                 for (String index : row) {
-                    System.out.printf("%-10s", index);
+                    System.out.printf("%-20s", index);
                 }
-                System.out.println(); // 
+                System.out.println();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -34,3 +40,4 @@ public class CSVReaderExample {
         }
     }
 }
+ 
