75dfaa094964fc09f532860f298ec79972045e90
Alvaro551 <92580240+Alvaro551@users.noreply.github.com>
2023-11-07 19:04:38 +0100

    Update IDatosJuego.java

--- lucaSteamProject/src/datos/IDatosJuego.java
+++ lucaSteamProject/src/datos/IDatosJuego.java
@@ -2,23 +2,5 @@ package datos;
 
 public interface IDatosJuego {
 	
-	/*
-	public void addEmpleado(Empleado p);
-	public void addEmpleado();
-
-	public int encontrarEmpleado(Empleado e);
-
-	public void eliminarEmpleado();
-	public void eliminarEmpleado(Empleado e);
 	
-	public boolean existeEmpleado();
-	public boolean existeEmpleado(Empleado e);
-
-	public void modificarEmpleado();
-	public void modificarEmpleado(Empleado e);
-
-	public void listado();
-	
-	public void CasosDePrueba();
-	*/
 }
