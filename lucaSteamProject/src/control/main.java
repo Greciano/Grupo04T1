8f3921822980c24627e554c4b994f0b9e21f53e4
Mori <morillas022@gmail.com>
2023-11-08 14:41:54 +0100

    versionMori

--- lucaSteamProject/src/control/main.java
+++ lucaSteamProject/src/control/main.java
@@ -7,7 +7,6 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		IDatosJuego archivo= new DatosJuegoImp();
 		archivo.leerYAlmacenarDatos("src/resources/vgsales.csv");
 		archivo.leerArchivo("src/resources/vgsales.csv");
