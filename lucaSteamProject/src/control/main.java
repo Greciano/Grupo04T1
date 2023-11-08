0ff4f71bff8b13ece0fff16283f1b9d9c9ba128c
Alvaro551 <92580240+Alvaro551@users.noreply.github.com>
2023-11-08 14:47:31 +0100

    Add files via upload

--- lucaSteamProject/src/control/main.java
+++ lucaSteamProject/src/control/main.java
@@ -1,20 +1,14 @@
 package control;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import datos.DatosJuegoImp;
+import datos.IDatosJuego;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		ManipularCSV archivo= new ManipularCSV();
+		IDatosJuego archivo= new DatosJuegoImp();
 		archivo.leerYAlmacenarDatos("src/resources/vgsales.csv");
 		archivo.leerArchivo("src/resources/vgsales.csv");
-
 	}
-
 }
