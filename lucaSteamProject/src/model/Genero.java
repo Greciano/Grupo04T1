80b6ffaabc625b5b61b2e0bf3c28edfc73956d20
Alvaro Medina Vena <alvaromedinavena11@gmail.com>
2023-11-08 11:58:25 +0100

    Version1.5

--- lucaSteamProject/src/model/Genero.java
+++ lucaSteamProject/src/model/Genero.java
@@ -1,5 +1,9 @@
 package model;
 
 public class Genero {
+	
+	  public enum genero {
+	        ACTION, SPORTS, SHOOTER, ROLE_PLAYING, PLATFORM, MISC, RACING, FIGHTING, SIMULATION, ADVENTURE, STRATEGY, PUZZLE
+	    }
 
 }
