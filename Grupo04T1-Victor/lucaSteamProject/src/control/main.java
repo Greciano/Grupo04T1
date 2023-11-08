1f49f2f05dc6a8d05cde19450a701e5d36076b62
Víctor Greciano <150051660+Greciano@users.noreply.github.com>
2023-11-08 14:24:49 +0100

    Add files via upload

---
+++ Grupo04T1-Victor/lucaSteamProject/src/control/main.java
@@ -0,0 +1,15 @@
+package control;
+
+import datos.DatosJuegoImp;
+import datos.IDatosJuego;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		IDatosJuego archivo= new DatosJuegoImp();
+		archivo.leerYAlmacenarDatos("src/resources/vgsales.csv");
+		archivo.leerArchivo("src/resources/vgsales.csv");
+	}
+}
