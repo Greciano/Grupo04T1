f21b0016178a669971eace9d2ad3a014d13b4d2c
Víctor Greciano <150051660+Greciano@users.noreply.github.com>
2023-11-09 12:59:14 +0100

    Delete Grupo04T1-Victor/lucaSteamProject directory

--- Grupo04T1-Victor/lucaSteamProject/src/model/Genero.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Genero {
-	
-	  public enum genero {
-	        ACTION, SPORTS, SHOOTER, ROLE_PLAYING, PLATFORM, MISC, RACING, FIGHTING, SIMULATION, ADVENTURE, STRATEGY, PUZZLE
-	    }
-
-}
